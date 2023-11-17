b00b6d8f76861fc38336d11eb37316fd38656275
goldenmat <matdelloro@gmail.com>
2023-11-17 17:49:27 +0100

    test php

---
+++ iluci/04102023/index.php
@@ -0,0 +1,270 @@
+<head>
+	<link rel="stylesheet" href="../style/style.css">
+	<script type="text/javascript" src="../script/script.js"></script>
+	<link rel="icon" type="image/x-icon" href="../assets/icon.png">
+	<title>I Luci! Moregallo - Cresta OSA</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<div class="container">
+	
+		<div class="banner-container"><img class="banner" src="../assets/banner/alpinismoroccia.png"></div>
+		
+		<!-- Descrizione -->
+		<div class="description">
+			<h2>Moregallo - Cresta OSA</h2>
+			
+			<a href="schizzo.png" target="_blank" rel="noopener noreferrer">
+				<img class="schizzo" src="schizzo.png">
+			</a>
+			<br><p class="caption">(clicca sull'immagine per ingrandirla)</p>
+			
+			<p>Il Moregallo è la montagna che cinge a Ovest il ramo Orientale del lago di Como, meno blasonata rispetto ad altre montagne che sovrastano il lago come Grigne e Resegone, offre comunque, oltre a un ottimo panorama dalla cima, percorsi per tutti i gusti dal semplice escursionista all'alpinista più esperto. Diverse sono le natura dei suoi versanti, dal solare versante Sud solcato da guglie che ricordano un po' la Grignetta, alla selvaggia valle delle Moregge a Nord percorsa da un interessante forra, fino al lato Est tempestato dalle cave e percorso da vie alpinistiche di difficoltà molto elevata.</p>
+			
+			<p>La Cresta G.G. OSA è la via più ripetuta di tutto il Triangolo Lariano, grazie alle sue difficoltà contenute e al suo snodarsi con logica impeccabile sul versante Sud della montagna. Ben visibile da Valmadrera, la via è stata aperta nel 1973 da quattro giovani valmadreresi e oggi è spesso percorsa dalle scuole di alpinismo (in primis quella di Valmadrera) e da principianti che vogliono mettersi alla prova su questo tipo di percorsi. Sull'intera cresta, il cui sviluppo non è da sottovalutare, si trovano solo 4 soste attrezzate e un chiodo di progressione, ma non se ne sente assolutamente il bisogno in quanto clessidre, spuntoni e piante abbondano su tutto il percorso.</p>
+		</div>
+		
+		<div class="picker-bar">
+			<div class="picker-choice"><div class="picker button roccia" id="relazione-button">Relazione</div></div>
+			<div class="picker-choice"><div class="picker button roccia" id="condizioni-button">Condizioni</div></div>
+			<div class="picker-choice"><div class="picker button roccia" id="racconti-button">Racconti</div></div>
+			<div class="picker-choice"><div class="picker button roccia" id="foto-button">Foto</div></div>
+		</div>
+		
+		<div class="content">
+		
+			<!-- Relazione -->
+			<div id="relazione" class="relazione">
+				<h3>Relazione</h3>
+				
+				<table class="info roccia">
+					<tr>
+						<th>Data Uscita</th>
+						<td>04-10-2023, Mercoledì</td>
+						<th>Area</th>
+						<td>Corni di Canzo</td>
+					</tr>
+					<tr>
+						<th>Luogo di Partenza</th>
+						<td>Frazione Belvedere - Valmadrera (LC)</td>
+						<th>Quota</th>
+						<td>
+							315m la partenza<br>
+							950m (circa) l'attacco della via<br>
+							1276m la cima del Moregallo
+						</td>
+					</tr>
+					<tr>
+						<th>Dislivello</th>
+						<td>961m</td>
+						<th>Tempi</th>
+						<td>6:00 ore (3:00 ore la via)</td>
+					</tr>
+					<tr>
+						<th>Esposizione</th>
+						<td>Sud</td>
+						<th>Difficoltà Tecnica</th>
+						<td>III+</td>
+					</tr>
+					<tr>
+						<th>Punti di Appoggio</th>
+						<td>Rifugio SEV a Pianezzo e Agriturismo Rusconi e Ristoro OSA a San Tomaso, ma son necessarie deviazione per raggiungerli.</td>
+						<th>Acqua</th>
+						<td>Fontana presso la cappelletta VARS e fonte a Sambrosera</td>
+					</tr>
+					<tr>
+						<th>Partecipanti</th>
+						<td colspan="3">
+							<img class="person" src="../assets/people/oracolo.png"> Oracolo
+						</td>
+					</tr>
+				</table>
+				
+				<div class="left"> 
+				
+					<div class="centered"><a href="04102023.gpx" target="_blank" download>
+						<img class="traccia" src="traccia.png">
+					</a></div>
+					<br><p class="caption">(clicca sull'immagine per scarica la traccia GPS)</p>
+				
+					<!-- <h4>Accesso</h4>
+					
+					<p>Da Lecco attraversare il ponte Kennedy portandosi a Malgrate; salire per la superstrada fino a quando, superato l'Obi, si entra a Valmadrera. Percorrere tutta la strada curvando a sinistra nei pressi della chiesa, per poi prendere a destra via Leopardi (indicazioni San Tomaso) che va seguita fino a un bivio dal quale si prende a destra via san Carlo Borromeo. Seguirla fino a quando la strada diventa solo per autorizzati, dove si può parcheggiare la macchina (pochi posti). Nel caso non ci fosse posto è possibile lasciarla più sotto (via Caduti per la Libertà) o addirittura anche in centro a Valmadrera.</p>
+					
+					<h4>Avvicinamento</h4>
+					
+					<div class="halved-grid">
+						<div class="halved-block-text">Dal primo parcheggio citato salire la carrareccia che ripida ma senza difficoltà porta fino alla Cappelletta VARS, crocevia dei sentieri sopra Valmadrera. Si prende il sentiero che passa sul fianco sinistro della cappelletta (indicazioni) che attraverso un lungo bosco, a tratti scivoloso a causa dei numerosi passaggi, restando sempre sul sentiero principale, porta comodamente fino alla fresca fonte di Sambrosera dove è possibile fare una pausa ristoratrice. Tornare brevemente sul sentiero e prendere a sinistra (indicazioni), inerpicandosi su sentiero ripido ma non difficile che attraversa il versante Sud del Moregallo. Giunti a una forcella (altre indicazioni), prendere a sinistra per traccia come da cartello aggirando un iniziale sperone sulla sinistra, e risalendo un'ultima zona franata (residui della frana del 2020) si arriva all'attacco della via.</div>
+						
+						<div class="halved-block-image"><a href="a1.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="a1.jpg">
+						</a></div>
+					</div>
+					
+					<h4>Via</h4>
+					
+					<p>La seguente relazione è solo uno dei modi possibili di percorrere la Cresta OSA, personalmente (Oracolo) essendo la via che ho ripetuto di più, avendola sopra casa, questo è il modo in cui negli anni ho avuto modo di verificare è il miglior mix tra velocità e sicurezza, ottimo soprattutto per portare delle persone alle prime armi. Ovvio che l'affollamento sulla via o le preferenze personali possono far cambiare il piano, ma se non si conoscesse proprio la via consiglio di fare così, evitando punti di sosta scomodi o di far conserva dove magari qualcuno alle prime armi potrebbe subire l'esposizione.</p>
+					
+					<div class="halved-grid">
+						<div class="halved-block-text"><b>L1:</b> salire lo sperone in verticale senza percorso obbligato. Arrivati ad una zona dove la verticalità cala, ma non l'esposizione, superare un paio di spuntoni a cavallo della cresta e portarsi sotto a un passaggio più ostico (clessidra, p. III+). Superarlo con un ristabilimento a sinistra e proseguire fino in cima allo sperone, dove si sosta comodamente su spuntoni.
+						<br>&#187; 50m, diverse clessidre (III p. III+)</div>
+						
+						<div class="halved-block-image"><a href="l1.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l1.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L2:</b> proseguire sul filo di cresta aggirando degli spuntoni verso sinistra fino a una piccola zona boscosa (possibile sosta alternativa alla S1 se si avesse abbastanza corda). Da qui salire in verticale su buone prese (più facile a destra, ma è consigliabile spostarsi a sinistra in massima esposizione, comunque molto appigliato). Proseguire ora seguendo la cresta con percorso meno obbligato, arrivando a una zona più rotta, dalla quale un canalino appoggiato verso destra porta a un ottimo punto di sosta nei pressi dell'evidente frana del 2020 (ottimo spuntone a sinistra sotto la guglia).
+						<br>&#187; 60m, (III, II)</div>
+						
+						<div class="halved-block-image"><a href="l2.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l2.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L3:</b> salire in spaccata tra la guglia e il continuo della cresta, e in massima esposizione portarsi sotto all'unico chiodo di progressione della via. Il passaggio va fatto spostandosi verso sinistra, e comunque non è più impegnativo di quello fatto in L1. Superato questo la cresta diventa più discontinua, ed è possibile tenere il filo quanto possibile (comunque difficoltà sul III) o aggirare i passaggi tramite sentierino sulla destra, a piacere. Dopo un po' la cresta si abbatte e si prosegue tra le piante, fino a un ottimo punto di sosta (2 anelli cementati) sulla destra del camino.
+						<br>&#187; 50m, 1 chiodo (III+, III, I)</div>
+						
+						<div class="halved-block-image"><a href="l3.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l3.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L4:</b> salire il camino, unto ma non troppo difficile, si fa bene in spaccata cercando nella parte alta le mani sul lato sinistro (mentre l'uscita è consigliabile effettuarla sulla destra). Sosta subito dopo il camino su ottima pianta. Due varianti possibili: il camino si può evitare sulla destra su roccia esposta ma facile (III), e dopo l'uscita si può proseguire per facile cresta ora meno esposta (II) fino a 2 fix con catena poco visibili, usati per calarsi sul versante Ovest dove son state aperte delle vie abbastanza impegnative. Non sostare sulla catena (scomoda) ma sullo spuntone su cui essa sorge, che è molto comodo.
+						<br>&#187; 15m/40m, sasso incastrato nel camino (III+ oppure III+, II)</div>
+						
+						<div class="halved-block-image"><a href="l4.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l4.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>Raccordo:</b> da qui conviene proseguire in conserva o slegati se le capacità lo consentono, la cresta rimane aerea ma l'arrampicata è molto facile. Si superano diversi saliscendi, continuando progressivamente a guadagnare dislivello fino al bosco. Da qui si segue il sentiero che sale fino a un'elevazione, dalla quale si scende verso destra sempre su sentiero, fino ad arrivare sotto alla "paretina".
+						<br>&#187; 200m circa (II, poi sentiero)</div>
+						
+						<div class="halved-block-image"><a href="r1.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="r1.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L5:</b> vi sono diversi modi di affrontare la paretina: a sinistra è più facile ma improteggibile (III+), sulla destra un po' più impegnativa con una mini clessidra e una fessurina per friend medio (IV+), mentre al centro oppone le maggiori difficoltà e si può proteggere solo con qualche friend nelle poche fessure che offre (V+). Superato il salto comunque breve si arriva a un comodo pulpito ghiaioso con un solido fittone infisso nel terreno dove è possibile far sicura. La paretina è comunque aggirabile sulla destra per sentiero.
+						<br>&#187; 10m, difficile da proteggere</div>
+						
+						<div class="halved-block-image"><a href="l5.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l5.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>Raccordo:</b> proseguire per sentiero fino allo spigolo finale che scende dalla cima. Salire per balze erbose appena a destra dello stesso, dove un canaletto dal brutto aspetto è l'inizio dell'ultima parte della via
+						<br>&#187; 100m circa (sentiero)</div>
+						
+						<div class="halved-block-image"><a href="r2.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="r2.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L6:</b> affrontare il canaletto direttamente: questo è l'unico punto della via dove la roccia è decisamente friabile, ma è molto corto poichè dopo 6-7m si può uscire sullo spigolo a sinistra dove la roccia torna a essere ottima. Passata un'ottima clessidra, si sale lo spigolo direttamente (o poco a sinistra) fino a una zona più erbosa dove si sale senza più difficoltà fino a due fix da collegare su uno spuntone.
+						<br>&#187; 45m (III, I)</div>
+						
+						<div class="halved-block-image"><a href="l6.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l6.jpg">
+						</a></div>
+						
+						<div class="halved-block-text"><b>L7:</b> l'ultimo tiro è sicuramente il più facile tecnicamente ma anche il più caratteristico. Si sale sull'anticima poco sopra la sosta, dove il passaggio si palesa: un ponte di roccia, largo poco meno di 1 metro, da superare camminando fino all'intaglio finale, che si supera con un facile passaggio di arrampicata (II) arrivando ad un fix finale con libro di vetta. Da lì per prato in poche decine di metri si arriva in cima. Nonostante il ponte non offra appunto difficoltà tecniche, rimane parecchio esposto, quindi dipende molto dall'abitudine all'esposizione della persona che lo percorre: ho visto arrampicatori molto bravi farlo a quattro zampe e principianti alle prime armi camminare come se fossero su un qualsiasi sentiero...
+						<br>&#187; 30m (I p. II)</div>
+						
+						<div class="halved-block-image"><a href="l7.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="l7.jpg">
+						</a></div>
+					</div>
+					
+					<h4>Discesa</h4>
+					
+					<div class="halved-grid">
+						<div class="halved-block-text">Il modo più veloce di scendere è dalla cima di scendere verso Est alla bocchetta di Sambrosera, da qui a destra nella valle Due Pile fino a ripercorrere l'avvicinamento dall'ultimo cartello prima della cresta OSA. Se si avesse tempo (io ho fatto così) è più consigliabile invece scendere dalla cresta Ovest (catene e staffe sulla prima parte, attenzione), passando dalla bocchetta di Moregge (ottimo panorama sul lago) e poi scendendo a sinistra per il sentiero che collega Pianezzo a Sambrosera, raggiugnendo quest'ultima e da qui tornando per il sentiero dell'avvicinamento.</div>
+						
+						<div class="halved-block-image"><a href="d1.jpg" target="_blank" rel="noopener noreferrer">
+							<img class="image" src="d1.jpg">
+						</a></div>
+					</div> -->
+
+					<?php
+					include '../relazioni/11_1_9/crestaosa/index.php';
+					?>
+				
+				</div>
+
+			</div>
+		
+			<!-- Condizioni -->
+			<div id="condizioni" class="condizioni hidden">
+				<h3>Condizioni</h3>
+				
+				<table class="info roccia">
+					<tr>
+						<th>Meteo e Condizioni</th>
+						<td>Serata nebbiosa ma non fredda. La roccia in via come al solito è ottima, a parte il canaletto che porta al ponte di roccia, quindi ordinaria amministrazione.</td>
+					</tr>
+					<tr>
+						<th>Difficoltà Percepita</th>
+						<td>F (Oracolo)</td>
+					</tr>
+					<tr>
+						<th>Giudizio Soggettivo</th>
+						<td>
+							<img class="star" src="../assets/star.png">
+							<img class="star" src="../assets/star.png">
+							<img class="star" src="../assets/star.png">
+							<img class="star" src="../assets/star.png">
+							<img class="star" src="../assets/emptystar.png"> (Oracolo)
+						</td>
+					</tr>
+					<tr>
+						<th>Materiale</th>
+						<td>NDA per alpinismo su roccia (imbrago, casco, 4-6 rinvii, cordini). Eventualmente qualche friend medio, anche se clessidre e spuntoni abbondano. Scarpette non necessarie (o eventualmente toglierle dopo il camino) e nel caso in cui il primo di cordata abbia sufficiente esperienza una mezza corda è sufficiente.</td>
+					</tr>
+					<tr>
+						<th>Orientamento</th>
+						<td>I sentieri son tutti segnati e la via è super logica, nel dubbio rimanere sul filo di cresta che non si sbaglia.</td>
+					</tr>
+					<tr>
+						<th>Chiodatura</th>
+						<td>Mancante su tutta la via, soste comprese a parte 3 (di cui una poco utile). Comunque visto la natura della via non c'è bisogno di niente che non offra già la roccia.</td>
+					</tr>
+				</table>
+			</div>
+		
+			<!-- Racconti -->
+			<div id="racconti" class="racconti hidden">
+				<h3>Racconti</h3>
+				
+				<div class="story left">
+				
+					<div class="profile-block">
+						<img class="profile" src="../assets/people/oracolo.png">
+						<br>Oracolo
+					</div>
+					
+					<div class="story-block roccia">
+						Giretto solitario in OSA post lavoro, parto presto e vado deciso fino all'attacco bypassando Sambrosera, nessun problema sulla via a parte il solito passo sulla paretina. Provo una variante a sx prima dei tiri finali ma poi torno sul facile, roccia marcissima. In alto nebbia e freschino, scendo con calma nel buio con la frontale. Comunque buono stato di forma tutto sommato.
+					</div>
+				</div>
+			</div>
+		
+			<!-- Foto -->
+			<div id="foto" class="foto hidden">
+				<h3>Foto</h3>
+				
+				<a href="https://photos.app.goo.gl/pDqw98YpcWvHSsJx7" target="_blank" rel="noopener noreferrer">
+					<img src="../assets/googlephotos.png">
+					<p>Google Photos Album</p>
+				</a>
+			</div>
+		</div>
+	
+		<a href="../">
+			<div class="back roccia">
+				<div class="back-button">
+					<img class="back-image" src="../assets/back.png">
+				</div>
+				<div class="back-text">
+					<p class="back-text">Home</p>
+				</div>
+			</div>
+		</a>
+	</div>
+
+</body>
